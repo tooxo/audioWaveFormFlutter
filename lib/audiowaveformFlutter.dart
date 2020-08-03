494c787606e89ca31acd7b7339b90c56b6c78a10
till <till@s.chulte.de>
2020-08-03 22:02:12 +0200

    made function private

--- lib/audiowaveformFlutter.dart
+++ lib/audiowaveformFlutter.dart
@@ -7,14 +7,14 @@ final DynamicLibrary nativeAddLib = Platform.isAndroid
     : DynamicLibrary.process();
 
 class AudiowaveformFlutter {
-  static final Pointer<Utf8> Function(Pointer<Utf8> fileName) nativeWaveForm =
+  static final Pointer<Utf8> Function(Pointer<Utf8> fileName) _nativeWaveForm =
       nativeAddLib
           .lookup<NativeFunction<Pointer<Utf8> Function(Pointer<Utf8>)>>(
               "extractWaveData")
           .asFunction();
 
   static String audioWaveForm(String inputPath) {
-    Pointer<Utf8> test = nativeWaveForm(Utf8.toUtf8(inputPath));
+    Pointer<Utf8> test = _nativeWaveForm(Utf8.toUtf8(inputPath));
     return Utf8.fromUtf8(test);
   }
 
